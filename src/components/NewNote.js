bd2017d70440fac3505c9afd443bc7bfa308196e
SorryImNoob <SorryImNoob@users.noreply.github.com>
2020-11-28 11:57:46 +0200

    fix

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -3,9 +3,9 @@
 function NewNote (props){
     return (
         <li key={props.id} id={props.id}>
-            <h4>{props.date}</h4>
-            <h1>{props.header}</h1>
-            <p>{props.body}</p>
+            <h4 id={props.id}>{props.date}</h4>
+            <h1 id={props.id}>{props.header}</h1>
+            <p id={props.id}>{props.body}</p>
         </li>
     )
 }
